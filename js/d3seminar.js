1dbc413cc10ef00a99095132d4ebfd01a9fe9f99
Alex O'Connor <Alex.OConnor@scss.tcd.ie>
2013-11-26 12:32:57 +0000

    fiddling with css on matrix text

--- js/d3seminar.js
+++ js/d3seminar.js
@@ -34,7 +34,7 @@ var svgslide = d3.select("#svgchart").append("svg:svg")
             .text("SVG"); //if I were to continue the chain here, it would append to text rather than svgslide
     
 /* Shapes */
-var shapeslide = d3.select("#shapes #shapechart").append("svg:svg")
+var shapeslide = d3.select("#shapechart").append("svg:svg")
                 .attr("width", slideWidth-100)
                 .attr("height", slideHeight / 3)
                 .attr("class", "chart")
@@ -44,5 +44,55 @@ var shapeslide = d3.select("#shapes #shapechart").append("svg:svg")
               .attr("cx", "350")
               .attr("cy", "150")
               .attr("r", "50")
-              .attr("fill", "#300");
+              .attr("fill", "#333");
 
+/* Animation */
+
+var animslidePos = d3.select("#animchartPos").append("svg:svg")
+                .attr("width", "100%")
+                .attr("height", "80%")
+                .attr("class", "chart")
+                .attr("class", "border");
+   
+    animslidePos.append("rect")
+                .attr("x", 5)
+                .attr("y", 5)
+                .attr("width", 50)
+                .attr("height", 50)
+                .attr("fill", "white");
+
+    //just select the rectangle in the svg
+    animslidePos.on("click", function(){
+        animslidePos.select("rect")
+        .transition()
+        .duration(1000)
+        .attr("x", "80%")
+        .attr("y", "75%")
+        .transition()
+        .delay(1000)
+        .attr("fill", "red");
+    });
+
+
+var animslideAdd = d3.select("#animchartAdd").append("svg:svg")
+                .attr("width", slideWidth /3 )
+                .attr("height", slideHeight / 2)
+                .attr("class", "chart")
+                .attr("class", "border");
+//the data array
+var animslideAddData = ["电","كهرباء","बिजली","전기","⚡"];
+
+    animslideAdd.on("click", function(){
+        alert("data");
+        animslideAdd.selectAll("text")
+        .data(animslideAddData)
+        .enter()
+        .append("text")
+        .attr("class","matrixText")
+        .attr("y", 0)
+        .attr("x", function(d,i){ return (10 + 20 * i); })
+        .text(function(d,i){console.log(d); return d;})
+        .transition()
+        .attr("y", "80%");
+        
+    });
